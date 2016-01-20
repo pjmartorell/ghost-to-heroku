c599fde5094b08e5c89d5ca57ccce32f3355ad08
Pere Joan Martorell <perejoan@itnig.net>
2016-01-20 03:40:54 +0100

    Add PM2

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-var path = require('path');
-var ghost = require('ghost');
+var pm2 = require('pm2');
 
-ghost({
-  config: path.join(__dirname, 'config.js')
-}).then(function (ghostServer) {
-  ghostServer.start();
+var MACHINE_NAME = 'hk1';
+var PRIVATE_KEY  = process.env.KM_PRIVATE_KEY;   // Keymetrics Private key
+var PUBLIC_KEY   = process.env.KM_PUBLIC_KEY;   // Keymetrics Public  key
+
+var instances = process.env.WEB_CONCURRENCY || -1; // Set by Heroku or -1 to scale to max cpu core -1
+var maxMemory = process.env.WEB_MEMORY      || 512;// " " "
+
+pm2.connect(function() {
+  pm2.start({
+    script    : 'server.js',
+    name      : 'production-app',     // ----> THESE ATTRIBUTES ARE OPTIONAL:
+    exec_mode : 'cluster',            // ----> https://github.com/Unitech/PM2/blob/master/ADVANCED_README.md#schema
+    instances : instances,
+    max_memory_restart : maxMemory + 'M',   // Auto restart if process taking more than XXmo
+    // env: {                            // If needed declare some environment variables
+    //   "NODE_ENV": "production",
+    //   "AWESOME_SERVICE_API_TOKEN": "xxx"
+    // },
+    post_update: ["npm install"]       // Commands to execute once we do a pull from Keymetrics
+  }, function() {
+    pm2.interact(PRIVATE_KEY, PUBLIC_KEY, MACHINE_NAME, function() {
+
+     // Display logs in standard output
+     pm2.launchBus(function(err, bus) {
+       console.log('[PM2] Log streaming started');
+
+       bus.on('log:out', function(packet) {
+        console.log('[App:%s] %s', packet.process.name, packet.data);
+       });
+
+       bus.on('log:err', function(packet) {
+         console.error('[App:%s][Err] %s', packet.process.name, packet.data);
+       });
+      });
+
+
+    });
+  });
 });
